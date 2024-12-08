58a63005ab9b501ebcec857599f665397202519f
Amelia2806 <ameliaayunitricahyanti2005@mail.ugm.ac.id>
2024-12-08 17:10:12 +0700

    update api service

--- app/src/main/java/com/example/uas_ppapb/network/ApiService.kt
+++ app/src/main/java/com/example/uas_ppapb/network/ApiService.kt
@@ -5,6 +5,8 @@ import retrofit2.Call
 import retrofit2.http.Body
 import retrofit2.http.GET
 import retrofit2.http.POST
+import retrofit2.http.PUT
+import retrofit2.http.Path
 
 interface ApiService {
 
@@ -12,6 +14,12 @@ interface ApiService {
     @GET("LIcJU/movies")
     fun getMovies(): Call<List<FilmUserData>>
 
+    @GET("LIcJU/movies/{id}")
+    fun getMovieDetails(@Path("id") movieId: String): Call<FilmUserData>
+
     @POST("LIcJU/movies")
     suspend fun createMovie(@Body movie: FilmUserData): retrofit2.Response<Void>
+
+    @PUT("LIcJU/movies/{id}")
+    suspend fun updateMovie(@Path("id") movieId: String, @Body movie: FilmUserData): retrofit2.Response<Void>
 }
